b6947450aadcb1021dff69c966acf9817581285b
LukaszKot <lukaszkot03@gmail.com>
2021-09-30 18:49:40 +0200

    Added table acad_positions.

--- database.sql
+++ database.sql
@@ -14,3 +14,8 @@ CREATE TABLE grades(
 CREATE TABLE departments(
 	department nvarchar PRIMARY KEY
 );
+
+CREATE TABLE acad_positions(
+	acad_position nvarchar PRIMARY KEY,
+	overtime_rate integer
+)
